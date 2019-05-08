19d784c965ffa35eb18a7f8d7fa1f850f3e09123
Gabriel Alexandre <gabriel.shaad@gmail.com>
2019-05-08 00:16:39 -0300

    Add components and redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import "./App.scss";
+import { Container, Section } from 'rbx';
+import Header from './components/header';
+import Routes from './router';
 
-const App = () => <p>iFood Clone</p>
+const App = () => (
+  <Fragment>
+    <Header />
+    <Section>
+      <Container>
+        <Routes />
+      </Container>
+    </Section>
+  </Fragment>
+)
 
 export default App;
